1d5484c4133f37eb2578bb325b982a2861237261
KwachOjunga <KwachOjunga@users.noreply.github.com>
2026-03-18 09:10:57 +0000

    deploy: a774762b8aec76affc8c4ab459fbd9c0e5700fed

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a17696a5.js',
+                path_to_root + 'searchindex-a19dc996.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
